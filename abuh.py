df08e6b10c2c9777f4a02c3a66b1190ec500c5ab
jalicdan-lang <jalicdan@carc.pshs.edu.ph>
2026-09-08 15:37:53 +0800

    Add files via upload

---
+++ abuh.py
@@ -0,0 +1,19 @@
+
+import math
+
+a = float(input("Enter the length of the side a: "))
+b = float(input("Enter the length of the side b: "))
+
+c = math.sqrt(math.pow(a,2) + math.pow(b,2))
+
+print(f"The hypotenuse is: {c:.2f}")
+
+
+
+
+
+
+
+
+
+
